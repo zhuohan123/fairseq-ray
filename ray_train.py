9f1095e6ff1f7c35f7fffa7cc36c0f6018f1a7e4
Zhuohan Li <zhuohan123@vip.qq.com>
2019-09-16 19:57:27 -0700

    detect cluster_resources

--- ray_train.py
+++ ray_train.py
@@ -308,6 +308,8 @@ def ray_main():
     retry = True
     while retry:
         ray.init(redis_address=args.redis_address)
+        cluster_resources = ray.cluster_resources()
+        args.distributed_world_size = cluster_resources["GPU"]
         Actor = ray.remote(num_cpus=1, num_gpus=int(not args.cpu))(RayDistributedActor)
         workers = [Actor.remote() for i in range(args.distributed_world_size)]
         ip = ray.get(workers[0].get_node_ip.remote())
